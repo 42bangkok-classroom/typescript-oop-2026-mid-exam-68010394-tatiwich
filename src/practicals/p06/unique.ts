065dda84a2e7c229028ac8d99db951bdf41a9aa4
Tatiwich <68010394@kmitl.ac.th>
2026-01-21 16:28:03 +0700

    Update unique.ts

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,4 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
-}
-
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+const number = string(process.argv[2]);
+function getUniqueNumbers(arr1: number[], arr2: number[]): number[] 
+const newArray: number[] = arr1.concat(arr2,);
+console.log(number.filter ("arr1 == arr2"));
