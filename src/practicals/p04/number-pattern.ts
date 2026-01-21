f4211a9e25d5aaee59cc9b4f4e2a643f307bb9fd
Tatiwich <68010394@kmitl.ac.th>
2026-01-21 16:13:54 +0700

    Update number-pattern.ts

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,19 @@
-// Write your code below
+const N = Number(process.argv[2]);
+
+if (isNaN(n) || n <= 0) {
+    console.log("");
+    process.exit(1);
+    }
+for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N - i; j++) {
+        let row = "";
+    }
+      
+    for (let j = 1; j <= 2 * i - 1; j++)  {
+        let row += j;
+            }
+    
+   
+    console.log(row);
+}
+
