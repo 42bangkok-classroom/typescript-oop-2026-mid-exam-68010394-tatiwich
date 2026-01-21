1c17e4b5f8da4fc73eaea8c1ce3caa24d026019c
Tatiwich <68010394@kmitl.ac.th>
2026-01-21 15:20:43 +0700

    Update grading.ts

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,20 @@
-// Write your code below
+const n = Number(process.argv[2]);
+
+if (n <= 100 && n >= 80){
+    console.log("Grade is A");
+}
+else if (n <= 79 && n >= 70){
+    console.log("Grade is B");
+}
+else if ( n <= 69 && n >= 60){
+    console.log("Grade is C");
+}
+else if ( n <= 59 && n >= 50){
+    console.log("Grade is D");
+}
+else if ( n <= 49 && n >= 0){
+    console.log("Grade is F");
+}
+else [
+    console.log("Invalid input");
+]
